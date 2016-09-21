d8023fded89e6cfd96ee79e6a4d9e52448e9f121
George Yumnam <georgeyumnam@gmail.com>
2016-09-21 16:52:19 +0530

    new

--- lab1/intro_C.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h> // standard input output library
-#include<stdlib.h> // standard library
-
-void main () {  // void is the return type of the function - void returns nothing
-                // there can be one main - any function named 'main' is compiled first by the compiler.
-
-printf("Be creative\n");
-
-}
